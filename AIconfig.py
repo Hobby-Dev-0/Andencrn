40db4091982fa4ba8f261cf8f540c38d7ff8b823
InterAnylastics <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-10 16:22:32 +0530

    APi

---
+++ AIconfig.py
@@ -0,0 +1,11 @@
+import os
+bot_token = os.get.environ.get("TOKEN", None)
+ARQ_API_KEY = os.get.environ.get("ARQ_API_KEY", None)
+
+# List of supported languages >>
+# https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
+
+LANGUAGE = "en"
+
+# Leave it as it is
+ARQ_API_BASE_URL = "https://thearq.tech"
