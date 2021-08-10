c9d9c205e3ee2d330610285ae048b31aa9cc2dfb
InterAnylastics <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-10 16:20:53 +0530

    memory session

---
+++ AI.py
@@ -0,0 +1,120 @@
+import os
+op = os.get.environ("AI_CHAT", None)
+if op == False or None:
+    return
+else:
+    pass
+import re
+from asyncio import gather, get_event_loop, sleep
+
+from aiohttp import ClientSession
+from pyrogram import Client, filters, idle
+from Python_ARQ import ARQ
+
+from AIconfig import ARQ_API_BASE_URL, ARQ_API_KEY, LANGUAGE, bot_token
+
+Andencento = Client(
+    ":memory:",
+    bot_token=bot_token,
+    api_id=6,
+    api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e",
+)
+
+bot_id = int(bot_token.split(":")[0])
+arq = None
+
+
+async def AndencentoQuery(query: str, user_id: int):
+    query = (
+        query
+        if LANGUAGE == "en"
+        else (await arq.translate(query, "en")).result.translatedText
+    )
+    resp = (await arq.Andencento(query, user_id)).result
+    return (
+        resp
+        if LANGUAGE == "en"
+        else (
+            await arq.translate(resp, LANGUAGE)
+        ).result.translatedText
+    )
+
+
+async def type_and_send(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id if message.from_user else 0
+    query = message.text.strip()
+    await message._client.send_chat_action(chat_id, "typing")
+    response, _ = await gather(AndencentoQuery(query, user_id), sleep(2))
+    await message.reply_text(response)
+    await message._client.send_chat_action(chat_id, "cancel")
+
+
+@Andencento.on_message(filters.command("repo") & ~filters.edited)
+async def repo(_, message):
+    await message.reply_text(
+        "[GitHub](https://github.com/thehamkercat/AndencentoChatBot)"
+        + " | [Group](t.me/PatheticProgrammers)",
+        disable_web_page_preview=True,
+    )
+
+
+@Andencento.on_message(filters.command("help") & ~filters.edited)
+async def start(_, message):
+    await Andencento.send_chat_action(message.chat.id, "typing")
+    await sleep(2)
+    await message.reply_text("/repo - Get Repo Link")
+
+
+@Andencento.on_message(
+    ~filters.private
+    & filters.text
+    & ~filters.command("help")
+    & ~filters.edited,
+    group=69,
+)
+async def chat(_, message):
+    if message.reply_to_message:
+        if not message.reply_to_message.from_user:
+            return
+        from_user_id = message.reply_to_message.from_user.id
+        if from_user_id != bot_id:
+            return
+    else:
+        match = re.search(
+            "[.|\n]{0,}Andencento[.|\n]{0,}",
+            message.text.strip(),
+            flags=re.IGNORECASE,
+        )
+        if not match:
+            return
+    await type_and_send(message)
+
+
+@Andencento.on_message(
+    filters.private & ~filters.command("help") & ~filters.edited
+)
+async def chatpm(_, message):
+    if not message.text:
+        return
+    await type_and_send(message)
+
+
+async def main():
+    global arq
+    session = ClientSession()
+    arq = ARQ(ARQ_API_BASE_URL, ARQ_API_KEY, session)
+
+    await Andencento.start()
+    print(
+        """
+-----------------
+| AI Started! |
+-----------------
+"""
+    )
+    await idle()
+
+
+loop = get_event_loop()
+loop.run_until_complete(main())
