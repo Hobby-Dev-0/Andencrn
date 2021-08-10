e26acd73a50598c20f9de50c144d025eb23466c3
InterAnylastics <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-10 16:34:18 +0530

    AI

--- AIconfig.py
+++ AIconfig.py
@@ -1,6 +1,6 @@
 import os
-bot_token = os.get.environ.get("TOKEN", None)
-ARQ_API_KEY = os.get.environ.get("ARQ_API_KEY", None)
+bot_token = os.environ.get("TOKEN", None)
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", None)
 
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
