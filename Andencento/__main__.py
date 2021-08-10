d23faac0dc3ddb98c22f176bb1a8a79f766ff48e
InterAnylastics <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-10 15:26:08 +0530

    hm

--- Andencento/__main__.py
+++ Andencento/__main__.py
@@ -29,7 +29,7 @@ from Andencento.modules.connection import connect_button
 PM_START_TEXT = """
 _Hello_ *{}*
 _My name is_ *{}*\n_A Powerful Telegram ProBot to Manage Your Groups,feel free to add to your groups!!_
-_Maintained by_ [{}](@Andencento)
+_Maintained by_ [Team Andencento](https://telegram.me/Andencento)
 """
 
 
