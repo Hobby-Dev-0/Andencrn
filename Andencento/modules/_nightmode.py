be2f8a49d4d355e0c9c84862f6c2c1ebcf96074d
Aman Kumar Pandey <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-12 09:06:52 +0530

    everyday

--- Andencento/modules/_nightmode.py
+++ Andencento/modules/_nightmode.py
@@ -151,7 +151,7 @@ async def job_open():
     for pro in chats:
         try:
             await tbot.send_message(
-              int(pro.chat_id), "06:00 Am, Group Is Opening.\n**Powered By Masha**"
+              int(pro.chat_id), "06:00 Am, Group Is Opening.\n**Powered By Andencento**"
             )
             await tbot(
             functions.messages.EditChatDefaultBannedRightsRequest(
